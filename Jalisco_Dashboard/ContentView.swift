1b5c6758f1447b543de1d9a03b90f935aa36fd04
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-03-29 10:29:50 -0600

    Initial Commit

---
+++ Jalisco_Dashboard/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Jalisco_Dashboard
+//
+//  Created by Leonardo David Arias Ruiz on 29/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jalisco_Dashboard/Jalisco_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jalisco_DashboardApp.swift
+//  Jalisco_Dashboard
+//
+//  Created by Leonardo David Arias Ruiz on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Jalisco_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
